5bb65f3b62f14dd6c6e63912daf0617e4f78def6
AleksanderTelecki <129111@student.uni.opole.pl>
2021-07-06 18:35:19 +0200

    Init commit

---
+++ Toggl API/APIHelper/Helper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toggl;
+using Toggl.Extensions;
+using Toggl.QueryObjects;
+
+namespace Toggl_API.APIHelper
+{
+    public class Helper
+    {
+        public static string APIToken { get => "c99e059b3a3bb073c14097be588d2cbd";}
+
+        public Toggl.Services.ProjectService ProjectService { get; private set; }
+
+        public Toggl.Services.TaskService TaskService { get ; private set; }
+
+        public Toggl.Services.TimeEntryService TimeEntryService { get ; private set; }
+
+        public  Toggl.Services.ClientService ClientService { get ; private set; }
+
+        public  Toggl.Services.WorkspaceService WorkspaceService { get ; private set; }
+
+
+
+        public Helper()
+        {
+
+            ProjectService = new Toggl.Services.ProjectService(APIToken);
+            TaskService = new Toggl.Services.TaskService(APIToken);
+            TimeEntryService = new Toggl.Services.TimeEntryService(APIToken);
+            ClientService = new Toggl.Services.ClientService(APIToken);
+            WorkspaceService = new Toggl.Services.WorkspaceService(APIToken);
+
+
+
+        }
+
+        public string GetClientProjectTimeTrack(string Name, string startDate, string endDate)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var prams = new TimeEntryParams();
+
+            prams.StartDate = Convert.ToDateTime(startDate);
+            prams.EndDate = Convert.ToDateTime(endDate);
+
+            var hours = TimeEntryService.List(prams);
+
+            var client = ClientService.GetByName(Name);
+            var clientproj = ProjectService.List().Where(w => w.ClientId == client.Id);
+
+
+
+
+            result.AppendLine($"{client.Name}");
+            foreach (var item in clientproj)
+            {
+                result.AppendLine($"   {item.Name}");
+                var choosedtimestamp = hours.Where(w => w.ProjectId == item.Id).ToList();
+                var choosedtasks = TaskService.ForProject((int)item.Id);
+                foreach (var timestamp in choosedtimestamp)
+                {
+                    DateTime start = DateTime.Parse(timestamp.Start, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                    DateTime stop = DateTime.Parse(timestamp.Stop, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                    timestamp.TaskName = GetTaskNameByID((int)timestamp.TaskId, choosedtasks);
+                  
+                    result.AppendLine($"({timestamp.TaskName}) Duration: {TimeSpan.FromSeconds((double)timestamp.Duration).TotalHours} h | {start.ToString("HH:mm")} - {stop.ToString("HH:mm")} | {timestamp.Description}");
+          
+
+                }
+
+
+            }
+
+
+            return result.ToString();
+
+
+        }
+
+        private string GetTaskNameByID(int Id, List<Toggl.Task> tasks)
+        {
+            var task = tasks.Where(w => w.Id == Id).ToList();
+            return task[0].Name;
+
+        }
+
+
+
+
+    }
+}
---
+++ Toggl API/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Toggl;
+using Toggl.Extensions;
+using Toggl.QueryObjects;
+using Toggl_API;
+using Toggl_API.APIHelper;
+
+
+
+namespace Toggl_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+         
+
+            var helper = new Helper();
+            Debug.WriteLine(helper.GetClientProjectTimeTrack("Klient 1", "07/05/2021", "07/07/2021"));
+
+            
+
+
+        }
+
+
+     
+
+
+
+
+
+
+        //WorkSpace
+        //var WSService = new Toggl.Services.WorkspaceService(apiKey);
+        //var listwork = WSService.List();
+        //var workspaceID = listwork[0].Id;
+
+
+        //Project
+        //var project = new Project
+        //{
+        //    IsBillable = true,
+        //    WorkspaceId = WorkspaceID,
+        //    Name = "Project 2",
+        //    IsAutoEstimates = false,
+        //    ClientId = clients[0].Id,
+        //    IsActive = true,
+        //};
+        //var act = PService.Add(project);
+
+
+        //Add Task
+        //TService.Add(new Toggl.Task
+        //{
+        //    IsActive = true,
+        //    Name = "Test 1",
+        //    EstimatedSeconds = 3600,
+        //    WorkspaceId = WorkspaceID,
+        //    ProjectId = ProjectID
+        //});
+
+
+
+        //Lastjob
+        //var TService = new Toggl.Services.TaskService(apiKey);
+
+
+        //var tasks = TService.ForProject((int)ProjectID);
+
+        //var TEService = new Toggl.Services.TimeEntryService(apiKey);
+        //var timeEntry = TEService.Add(new TimeEntry()
+        //{
+        //    IsBillable = true,
+        //    CreatedWith = "TogglAPI.Net",
+        //    Duration = 900,
+        //    Start = DateTime.Now.ToIsoDateStr(),
+        //    WorkspaceId = WorkspaceID,
+        //    ProjectId=ProjectID,
+        //    TaskId = tasks[1].Id
+        //});
+
+
+
+        //var TService = new Toggl.Services.TaskService(apiKey);
+        //var task = TService.ForProject((int)ProjectID);
+        //var loadedTask = TService.Get((int)task[0].Id);
+        //loadedTask.IsActive = false;
+        //var editedTask = TService.Edit(loadedTask);
+
+    }
+}
